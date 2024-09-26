acfc2e4b9677fdd08de2891ee4289da28c710863
David Sekulic <sekulicdd@gmail.com>
2024-09-26 14:28:53 +0200

    update

--- assets/script.js
+++ assets/script.js
@@ -10,4 +10,31 @@ document.addEventListener('DOMContentLoaded', () => {
             menuToggle.checked = false;
         });
     });
+
+    document.getElementById("mailForm").addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        
+        var formData = new FormData(this);
+        var button = document.getElementById('submit');
+        var statusDiv = document.getElementById("mailStatus");
+        statusDiv.innerHTML='<span class="loader"></span>'
+      
+        button.disabled = true;
+        fetch("https://script.google.com/macros/s/AKfycbydhYTboR0xvDRfEIJND767RxpMwpXW-AMmb8wNSYNpIlS6DV1J6zK0zW6_K9y-1prl/exec", {
+            mode: "no-cors",
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.text())
+            .then(result => {
+                document.getElementById("mailForm").reset();
+                statusDiv.innerHTML = "<p>Poruka uspesno poslata</p>";
+            button.disabled = false;
+        })
+            .catch(error => {
+                statusDiv.innerHTML = "<p>Doslo je do greske</p>"
+                button.disabled = false;
+            });
+    });
+
 });
