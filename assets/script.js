e92258e95e4b95e2b3f71d10247603a1b2929b73
David Sekulic <sekulicdd@gmail.com>
2024-11-15 20:44:34 +0100

    update

--- assets/script.js
+++ assets/script.js
@@ -11,30 +11,55 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    document.getElementById("mailForm").addEventListener("submit", function(event) {
-        event.preventDefault(); 
+    // document.getElementById("mailForm").addEventListener("submit", function(event) {
+    //     event.preventDefault(); 
         
-        var formData = new FormData(this);
-        var button = document.getElementById('submit');
-        var statusDiv = document.getElementById("mailStatus");
-        statusDiv.innerHTML='<span class="loader"></span>'
+    //     var formData = new FormData(this);
+    //     var button = document.getElementById('submit');
+    //     var statusDiv = document.getElementById("mailStatus");
+    //     statusDiv.innerHTML='<span class="loader"></span>'
       
-        button.disabled = true;
-        fetch("https://script.google.com/macros/s/AKfycbydhYTboR0xvDRfEIJND767RxpMwpXW-AMmb8wNSYNpIlS6DV1J6zK0zW6_K9y-1prl/exec", {
-            mode: "no-cors",
-            method: "POST",
-            body: formData
-        })
-        .then(response => response.text())
-            .then(result => {
-                document.getElementById("mailForm").reset();
-                statusDiv.innerHTML = "<p>Poruka uspesno poslata</p>";
-            button.disabled = false;
-        })
-            .catch(error => {
-                statusDiv.innerHTML = "<p>Doslo je do greske</p>"
-                button.disabled = false;
-            });
-    });
+    //     button.disabled = true;
+    //     fetch("https://script.google.com/macros/s/AKfycbydhYTboR0xvDRfEIJND767RxpMwpXW-AMmb8wNSYNpIlS6DV1J6zK0zW6_K9y-1prl/exec", {
+    //         mode: "no-cors",
+    //         method: "POST",
+    //         body: formData
+    //     })
+    //     .then(response => response.text())
+    //         .then(result => {
+    //             document.getElementById("mailForm").reset();
+    //             statusDiv.innerHTML = "<p>Poruka uspesno poslata</p>";
+    //         button.disabled = false;
+    //     })
+    //         .catch(error => {
+    //             statusDiv.innerHTML = "<p>Doslo je do greske</p>"
+    //             button.disabled = false;
+    //         });
+    // });
+
+
+    var $carousel = $('.carousel').flickity({
+        imagesLoaded: true,
+        percentPosition: false,
+    
+      });
+      
+      var $imgs = $carousel.find('.carousel-cell img');
+      // get transform property
+      var docStyle = document.documentElement.style;
+      var transformProp = typeof docStyle.transform == 'string' ?
+        'transform' : 'WebkitTransform';
+      // get Flickity instance
+      var flkty = $carousel.data('flickity');
+      
+      $carousel.on( 'scroll.flickity', function() {
+        flkty.slides.forEach( function( slide, i ) {
+          var img = $imgs[i];
+          var x = ( slide.target + flkty.x ) * -1/2;
+          img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+        });
+      });
+      
 
 });
+
